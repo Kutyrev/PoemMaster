f663a879d488032d2f8664ffc48657b3effcbb7c
Kutyrev <42737353+Kutyrev@users.noreply.github.com>
2026-07-22 08:28:57 +0300

    Libs update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.github.kutyrev.poemmaster"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1"
+        versionCode = 11
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
